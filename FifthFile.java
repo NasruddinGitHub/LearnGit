820b53eca218c51db9be9fe5498eea04918446e2
Nasruddin Shaik <sknasruu@gmail.com>
2021-08-05 22:48:27 +0530

    Revert commit

--- FifthFile.java
+++ FifthFile.java
@@ -1,11 +1,5 @@
 public class FifthFile{
 	public static void main(String[] args) {
-		System.out.println("First Sysout");
-		System.out.println("Second Sysout");
-		System.out.println("Third Sysout");
-		System.out.println("Fifth Sysout");
-
-		System.out.println("Fourth Sysout");
-
+		System.out.println("Reverting the changes");
 	}
 }
